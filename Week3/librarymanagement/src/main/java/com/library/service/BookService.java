19508378678dc891d7c5df0788d06c3a25126080
Mayukh <mayukhm796@gmail.com>
2025-07-06 00:07:37 +0530

    Week3 codes

---
+++ Week3/librarymanagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,18 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+    private BookRepository bookRepository;
+
+    // Constructor injection
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    // Setter injection
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+}
---
+++ Week3/orm-learn/orm-learn/src/main/java/com/cognizant/orm_learn/service/CountryService.java
@@ -0,0 +1,35 @@
+package com.cognizant.orm_learn.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.cognizant.orm_learn.model.Country;
+import com.cognizant.orm_learn.repository.CountryRepository;
+import com.cognizant.orm_learn.service.exception.CountryNotFoundException;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+    
+    @Transactional
+    public List<Country> getAllCountries() {
+        return countryRepository.findAll();
+    }
+    @Transactional
+        public Country findCountryByCode(String countryCode) throws CountryNotFoundException {
+            Optional<Country> result = countryRepository.findById(countryCode);
+            if (!result.isPresent()) {
+                throw new CountryNotFoundException("Country not found");
+            }
+            return result.get();
+        }
+    @Transactional
+    public void addCountry(Country country) {
+        countryRepository.save(country);
+    }
+    }
